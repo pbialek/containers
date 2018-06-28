35149b2c550a1d2a21296659acddbb77650a0a53
pbialek <piotr.bialek@nokia.com>
2018-06-29 01:26:05 +0200

    Add templates to forward_list.

--- forward_list.cpp
+++ forward_list.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 
+template <class T>
 class Node {
     private:
         Node *next = 0;
-        int val = 0;
+        T val = 0;
 
 	public:
-		Node(int _val) {
+		Node(T _val) {
             val = _val;
         };
         Node() {};
@@ -14,31 +15,32 @@ class Node {
             next = _next;
         };
         Node *getNext() {return next;};
-        int getValue() { return val;};
+        T getValue() { return val;};
 };
 
+template <class T>
 class List {
     private:
         int size = 0;
-	    Node *front = 0;
+	    Node<T> *front = 0;
 
 	public:
-		List(int val) {
-            front = new Node(val);
+		List(T val) {
+            front = new Node<T>(val);
             size++;
         };
         List() {};
 
-        void push_front(int val) {
-            Node *temp = front;
-            front = new Node(val);
+        void push_front(T val) {
+            Node<T> *temp = front;
+            front = new Node<T>(val);
             front->setNext(temp);
         }
 
         int whatSize() { return size;};
 
         void showList() {
-            Node *temp = front;
+            Node<T> *temp = front;
             while(temp) {
                 std::cout << temp->getValue() << "\n";
                 temp = temp->getNext();
@@ -48,9 +50,14 @@ class List {
 
 int main()
 {
-    List l(5);
+    List<int> l(5);
     l.push_front(7);
     l.push_front(9);
 
     l.showList();
+
+    List<double> k(2.2);
+    k.push_front(1.9);
+    k.push_front(12.23);
+    k.showList();
 }
