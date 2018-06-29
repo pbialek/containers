aebeaad54062886dc1703e87acec5b5330f3340e
pbialek <piotr.bialek@nokia.com>
2018-06-29 02:18:58 +0200

    Use shared_pointers.

--- forward_list.cpp
+++ forward_list.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
+#include <memory>
 
 template <class T>
 class Node {
     private:
-        Node *next = 0;
+        std::shared_ptr<Node> next = 0;
         T val = 0;
 
     public:
@@ -11,10 +12,10 @@ class Node {
             val = _val;
         };
         Node() {};
-        void setNext(Node *_next) {
+        void setNext(std::shared_ptr<Node> _next) {
             next = _next;
         };
-        Node *getNext() {return next;};
+        std::shared_ptr<Node> getNext() {return next;};
         T getValue() { return val;};
 };
 
@@ -22,25 +23,25 @@ template <class T>
 class List {
     private:
         int size = 0;
-	    Node<T> *front = 0;
+        std::shared_ptr<Node<T>> front = 0;
 
     public:
         List(T val) {
-            front = new Node<T>(val);
+            front = std::shared_ptr<Node<T>>(new Node<T>(val));
             size++;
         };
         List() {};
 
         void push_front(T val) {
-            Node<T> *temp = front;
-            front = new Node<T>(val);
+            std::shared_ptr<Node<T>> temp = front;
+            front = std::shared_ptr<Node<T>>(new Node<T>(val));
             front->setNext(temp);
         }
 
         int whatSize() { return size;};
 
         void showList() {
-            Node<T> *temp = front;
+            std::shared_ptr<Node<T>> temp = front;
             while(temp) {
                 std::cout << temp->getValue() << "\n";
                 temp = temp->getNext();
